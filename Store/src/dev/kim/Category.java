0a7b6755838dc3d73687cb1b3bf1487f2e0d868e
Gideonkim96 <ronogideon97@gmail.com>
2024-02-29 13:15:19 +0300

    store

---
+++ Store/src/dev/kim/Category.java
@@ -0,0 +1,3 @@
+package dev.kim;
+
+public enum Category {PRODUCE, DAIRY, CEREAL, MEAT, BEVERAGE}
---
+++ Store/src/dev/kim/Product.java
@@ -0,0 +1,4 @@
+package dev.kim;
+
+public record Product(String sku, String name, String mfgr, Category category) {
+}
