bcfe57482a98826aa73f10d6a5741a7542bad6d6
Gideonkim96 <ronogideon97@gmail.com>
2024-03-08 11:35:50 +0300

    Thread

---
+++ Synchronization/src/dev/kim/Main.java
@@ -0,0 +1,38 @@
+package dev.kim;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BankAccount companyAccount = new BankAccount("Tom", 10000);
+
+        Thread thread1 = new Thread(() -> companyAccount.withdraw(2500));
+        Thread thread2 = new Thread(() -> companyAccount.deposit(5000));
+        Thread thread3 = new Thread(() -> companyAccount.setName("Kim"));
+        Thread thread4 = new Thread(() -> companyAccount.withdraw(5000));
+        Thread thread5 = new Thread(() -> companyAccount.deposit(5000));
+
+        thread1.start();
+        thread2.start();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        thread3.start();
+        thread4.start();
+        thread5.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+            thread3.join();
+            thread4.join();
+            thread5.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Final Balance: " + companyAccount.getBalance());
+    }
+}
